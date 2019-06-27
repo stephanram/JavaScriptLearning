a7501e324c0351c9869d7dba9a9be7151e128b7a
Stephan Thinagaran <stephan.thinagaran@aspiresys.com>
2019-06-27 16:24:58 +0530

    reduce

--- higherOrderFunction.js
+++ higherOrderFunction.js
@@ -1,15 +1,19 @@
+/*
 var arraySource = [
   { name: "Stephan", role: "SE" },
   { name: "Satkhi", role: "DB" },
   { name: "Azar", role: "QA" }
 ];
 
+//---------------------------------------------------------------
+//Filter Functions
 //Anonymous funciton
-var isQA = function(item) { return item.role === "QA";};
+var isQA = function (item) { return item.role === "QA"; };
 //Explicit return
-var isQAArrowFunction = (item) => { return item.role === "QA";};
+var isQAArrowFunction = (item) => { return item.role === "QA"; };
 //Implict return
 var isQAArrowFunctionUpdated = item => item.role === "QA";
+//---------------------------------------------------------------
 
 var result = arraySource.filter(isQAArrowFunctionUpdated);
 
@@ -18,9 +22,58 @@ var result = arraySource.filter(isQAArrowFunctionUpdated);
 //---------------------------------------------------------------
 //Mapping Functions
 
-var mapAsProfessional = (item) => { return { 'FirstName' : item.name, 'Role' : item.role }  } 
+var mapAsProfessional = (item) => { return { 'FirstName': item.name, 'Role': item.role } }
 var mappedResult = arraySource.map(mapAsProfessional);
-console.log(typeof(arraySource));
-console.log(typeof(mappedResult));
+console.log(typeof (arraySource));
+console.log(typeof (mappedResult));
 console.log(mappedResult);
+//---------------------------------------------------------------
+
+
+//---------------------------------------------------------------
+//Reduce function
+var orders = [
+  { amount: 100 },
+  { amount: 200 },
+  { amount: 300 },
+  { amount: 400 },
+  { amount: 500 },
+];
+
+var totalAmount = orders.reduce(function (sum, order) {
+  return sum + order.amount;
+}, '');
+
+var arrowTotalAmount = orders.reduce((sum, order) => { return sum + order.amount.toString(); }, '');
+var arrowTotalAmountUpdated = orders.reduce((sumArray, order) => { sumArray.push(order.amount.toString()); return sumArray; }, []);
+
+console.log(arrowTotalAmountUpdated);
+//--------------------------
+*/
+
+var fs = require('fs');
+
+var output = fs.readFileSync('SampleJson.txt', 'utf8')
+  .trim()
+  .split('\r\n')
+  .map(line => line.split('\t'))
+  .reduce((result, record) => {
+    result[record[0]] = result[record[0]] || []
+    result[record[0]].push(
+      {
+        name: record[0],
+        Designation: record[1],
+        Sarlary: record[2],
+        ResourceCount: record[3]
+      }
+    )
+    return result;
+  }, {})
+  ;
+
+
+console.log(JSON.stringify(output, null, 2));
+
+
+
 //---------------------------------------------------------------
